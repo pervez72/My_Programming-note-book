aedc6960b79764a1dbf040fe9e40ed2c1ec81682
pervez <phparvez71@gmail.com>
2024-03-19 23:10:13 +0600

    problem solved

--- Problem Codef/219774-H.cpp
+++ Problem Codef/219774-H.cpp
@@ -5,36 +5,39 @@ int main(){
 
    int n;
    cin>>n;
-   if (n>0&&n<1000)
+   if (n>0 && n<1000)
    {
       int a[n];
-      for (int i = 0; i <n; i++)
+      for (int i=0; i<n; i++)
       {
          cin>>a[i];
-         if(a[i]>=-100 &&a[i]>=100){
+         if(a[i]>=-100 &&a[i]<=100){
             a[i]=a[i];
          }
       } //input section ok
 
 
-      for (int step=0; step<n; step++)
+      for (int step=0; step<n-1; step++)
       {
-      for (int i=0;i<n; i++)
+      for (int i=0;i<n-step-1; i++)
       {
          if (a[i]>a[i+1])
          {
             int temp=a[i];
-            a[i+1]=temp;
             a[i]=a[1+i];
+            a[i+1]=temp;
+            
          }
          
       }
       
       }
 
-      for (int i= 0; i<n; i++)
+      for (int i=0; i<n; i++)
       {
-         cout<<a[i];
+         cout<<a[i]<<" ";
+       
+         
       }
       
    }    
