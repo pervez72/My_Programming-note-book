6122bd53191be1ea60273592c435c7030e091aea
pervez <phparvez71@gmail.com>
2024-03-21 11:26:19 +0600

    unsolved problem

---
+++ Problem Codef/unsolved-I.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int tt,n;
+    cin>>tt;
+    while (tt--) //test case 
+    {
+    cin>>n;
+    if (n>=2 && n<=100)
+    {
+        int a[n];
+      for (int  i = 0; i<n; i++) //get input loop
+    {
+        cin>>a[i];
+        if(a[i]>=-pow(10,6) && a[i]<=pow(10,6)){
+            a[i]=a[i]
+        }
+    } 
+    } //first test case input colsed
+
+    for (int i=0; i<n; i++)
+    {
+       i++;
+       for (int i = 0; i < count; i++)
+       {
+        s
+       }
+       
+    }
+    
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+    
+
+
+
+
+}
